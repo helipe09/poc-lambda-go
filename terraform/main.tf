ddb39fbde66f3293a59bc70c62b5702e40f763d0
helipe09 <37268964+helipe09@users.noreply.github.com>
2025-04-21 22:38:48 -0300

    fix/ajustes terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,37 @@
 resource "aws_lambda_function" "my_lambda" {
-  function_name = var.lambda_function_name
-  handler       = "main"
-  runtime       = "go1.x"
-  role          = aws_iam_role.lambda_exec.arn
-  source_code_hash = filebase64sha256("src/main.zip")
-  filename      = "src/main.zip"
+  function_name    = "go-lambda-function"
+  filename         = "../bin/function.zip"
+  source_code_hash = filebase64sha256("../bin/function.zip")
+  handler          = "bootstrap"
+  runtime          = "provided.al2"
+  role             = aws_iam_role.lambda_role.arn
 
   environment {
-    KEY = "value"
+    variables = {
+      ENV = "production"
+    }
   }
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "${var.lambda_function_name}-exec"
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
+        Effect = "Allow"
         Principal = {
           Service = "lambda.amazonaws.com"
         }
-        Effect = "Allow"
-        Sid    = ""
-      },
+      }
     ]
   })
 }
 
-resource "aws_iam_policy_attachment" "lambda_policy" {
-  name       = "${var.lambda_function_name}-policy-attachment"
-  roles      = [aws_iam_role.lambda_exec.name]
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,9 +12,11 @@ resource "aws_lambda_function" "my_lambda" {
 }
 
 output "lambda_function_arn" {
-  value = aws_lambda_function.my_lambda.arn
+  value       = aws_lambda_function.my_lambda.arn
+  description = "ARN da função Lambda"
 }
 
 output "lambda_function_name" {
-  value = aws_lambda_function.my_lambda.function_name
+  value       = aws_lambda_function.my_lambda.function_name
+  description = "Nome da função Lambda"
 }
