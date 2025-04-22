6205abe60f64ed61393024355f9d534f5d5b6a70
helipe09 <37268964+helipe09@users.noreply.github.com>
2025-04-21 23:02:44 -0300

    remover saída do ARN da função Lambda e ajustar a estrutura de saída no Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,3 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-output "lambda_function_arn" {
-  value = aws_lambda_function.my_lambda.arn
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,9 @@
-resource "aws_lambda_function" "my_lambda" {
-  function_name = var.lambda_function_name
-  handler       = "main"
-  runtime       = "go1.x"
-  role          = aws_iam_role.lambda_exec.arn
-  filename      = "path/to/your/deployment/package.zip"
-  source_code_hash = filebase64sha256("path/to/your/deployment/package.zip")
-
-  environment {
-    key   = "value"
-  }
-}
-
 output "lambda_function_name" {
   value       = aws_lambda_function.my_lambda.function_name
   description = "Nome da função Lambda"
+}
+
+output "lambda_function_arn" {
+  value       = aws_lambda_function.my_lambda.arn
+  description = "ARN da função Lambda"
 }
