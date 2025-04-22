6397467477c50a316c913ca541d03ccd392afd49
helipe09 <37268964+helipe09@users.noreply.github.com>
2025-04-21 23:01:07 -0300

    remover saída do ARN da função Lambda no Terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,11 +11,6 @@ resource "aws_lambda_function" "my_lambda" {
   }
 }
 
-output "lambda_function_arn" {
-  value       = aws_lambda_function.my_lambda.arn
-  description = "ARN da função Lambda"
-}
-
 output "lambda_function_name" {
   value       = aws_lambda_function.my_lambda.function_name
   description = "Nome da função Lambda"
